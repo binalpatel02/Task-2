6697eacccca964edf8614a8d7b4f6ec7435db1ee
binalpatel02 <binal.patel@saeculumsolutions.com>
2026-09-11 18:54:33 +0530

    create topics user, product, order

---
+++ app/order-service/src/library/kafka/topic.ts
@@ -0,0 +1,23 @@
+export const USER_TOPICS = {
+    CREATED: "user.created",
+    UPDATED: "user.updated",
+    DELETED: "user.deleted"
+};
+
+export const PRODUCT_TOPICS = {
+    CREATED: "product.created",
+    UPDATED: "product.updated",
+    DELETED: "product.deleted"
+};
+
+export const ORDER_TOPICS = {
+    CREATED: "order.created",
+    UPDATED: "order.updated",
+    DELETED: "order.deleted"
+};
+
+export const USER_CONSUMER_GROUP = "order-service-user-group";
+
+export const PRODUCT_CONSUMER_GROUP = "order-service-product-group";
+
+export const ORDER_CONSUMER_GROUP = "order-service-group";
