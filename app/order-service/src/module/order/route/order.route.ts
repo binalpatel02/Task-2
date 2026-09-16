f9273fa9fe43f017e6191ae6ec20cf4ec4d0dc5f
binalpatel02 <binal.patel@saeculumsolutions.com>
2026-09-16 17:16:24 +0530

    add orderRateLimit

--- app/order-service/src/module/order/route/order.route.ts
+++ app/order-service/src/module/order/route/order.route.ts
@@ -1,12 +1,16 @@
 import { Router } from "express";
-
 import { createOrderController, getOrdersController, getOrderByIdController, updateOrderController, deleteOrderController } from "../controller/order.controller.js";
-
-import { authenticate } from "@library/shared";
+import {rateLimit, authenticate } from "@library/shared";
 
 const router = Router();
 
-router.use(authenticate);
+const orderRateLimit = rateLimit({
+    windowSeconds: 60,
+    maxRequests: 30,
+    keyPrefix: "order-api"
+})
+
+router.use(authenticate, orderRateLimit);
 
 router.post("/", createOrderController);
 
