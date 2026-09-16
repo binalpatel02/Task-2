97091acfff8dcd399d1d1a4d62cadfec6bdeb63d
binalpatel02 <binal.patel@saeculumsolutions.com>
2026-09-16 14:02:43 +0530

    add login ratelimit

--- app/user-service/src/module/user/route/user.route.ts
+++ app/user-service/src/module/user/route/user.route.ts
@@ -1,9 +1,16 @@
 import { Router } from "express";
 import { createUserController, getUserByIdController, getUsersController, updateUserController, deleteUserController, loginUserController } from "../controller/user.controller.js";
+import { rateLimit } from "@library/shared";
 
 const router = Router();
 
-router.post("/login", loginUserController);
+const loginRateLimit = rateLimit({
+    windowSeconds: 300,
+    maxRequests: 5,
+    keyPrefix: "user-login",
+});
+
+router.post( "/login", loginRateLimit, loginUserController );
 router.post("/", createUserController);
 router.get("/", getUsersController);
 router.get("/:id", getUserByIdController);
