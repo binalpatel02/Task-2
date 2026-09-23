07f038d4b23c186d551090e5099309f8b0a2ab5f
binalpatel02 <binal.patel@saeculumsolutions.com>
2026-09-23 12:57:17 +0530

    create AbstractController

---
+++ library/shared/src/abstract/controller/index.ts
@@ -0,0 +1,18 @@
+export abstract class AbstractController {
+    protected success<T> (
+        data: T,
+        statusCode: 200
+    ) {
+        return {
+            statusCode,
+            data
+        };
+    }
+
+    protected created<T> (data: T) {
+        return {
+            statusCode: 201,
+            data
+        };
+    }
+}
