2639c45baaed426323129ff9f2db23e32f58b89b
binalpatel02 <binal.patel@saeculumsolutions.com>
2026-09-23 12:58:28 +0530

    create IServiceQuery,  IService, IServiceModel

---
+++ library/shared/src/abstract/interface/service/index.ts
@@ -0,0 +1,50 @@
+export interface IServiceQuery {
+    filter?: Record<string, unknown>;
+    page?: number;
+    limit?: number;
+    sort?: Record<string, 1 | -1>;
+}
+
+export interface IService<T, Id=string> {
+    create(
+        data: Partial<T>
+    ): Promise<T>
+
+    getById(
+        id: Id
+    ): Promise<T | null>
+
+    getAll(
+        query? : IServiceQuery
+    ): Promise<T[]>
+
+    update(
+        id: Id,
+        data: Partial<T>
+    ): Promise<T | null>
+
+    delete(
+        id: Id
+    ): Promise<T | null>;
+}
+
+export interface IServiceModel<T> {
+    add(data: Partial<T>): Promise<T>;
+
+    get(
+        filter: Record<string, unknown>
+    ): Promise<T | null>;
+
+    getAll(
+        filter?: Record<string, unknown>
+    ): Promise<T[]>;
+
+    update(
+        filter: Record<string, unknown>,
+        data: Partial<T>
+    ): Promise<T | null>;
+
+    delete(
+        filter: Record<string, unknown>
+    ): Promise<T | null>;
+}
