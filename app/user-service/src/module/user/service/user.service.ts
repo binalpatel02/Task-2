4a2ab86d27dace90b4383db04c29b9f962376741
binalpatel02 <binal.patel@saeculumsolutions.com>
2026-09-24 12:47:12 +0530

    add AbstractService

--- app/user-service/src/module/user/service/user.service.ts
+++ app/user-service/src/module/user/service/user.service.ts
@@ -3,171 +3,191 @@ import jwt from "jsonwebtoken";
 import { publishKafkaMessage } from "@library/third-party/kafka";
 import { USER_TOPICS } from "../../../library/kafka/topic.js";
 import { userModel } from "../model/user.model.js";
+import { AbstractService } from "@library/shared";
 
 interface ILoginRequest {
     email: string;
     password: string;
 }
 
-export const loginUser = async ({ email, password }: ILoginRequest) => {
-
-    const user = await userModel.getUserForLogin(email);
-    if (!user) {   
-        throw new Error("Invalid email");
+export class UserService extends AbstractService<any> {
+    
+    constructor() {
+        super(userModel, "_id")
     }
 
-    if (!user.password_hash) {
-        throw new Error("Password hash not found for this user");
-    }
-
-    const isPasswordValid = await bcrypt.compare(
-        password,
-        user.password_hash
-    );
-
-    if (!isPasswordValid) {
-        throw new Error("Invalid password");
-    }
-
-    const JWT_SECRET = process.env.JWT_SECRET;
-
-    if (!JWT_SECRET) {
-        throw new Error("JWT_SECRET is not configured");
-    }
-
-    const token = jwt.sign(
-        {
-            user_id: user._id,
-            email: user.email
-        },
-        JWT_SECRET,
-        {
-            expiresIn: "1d"
+    // LOGIN
+    async loginUser({ email, password }: ILoginRequest) {
+        const user = await userModel.getUserForLogin(email);
+        if (!user) {   
+            throw new Error("Invalid email");
         }
-    );
 
-    return {
-        access_token: token,
-        token_type: "Bearer",
-        expires_in: "1d",
-        user: {
-            user_id: user._id,
-            email: user.email
+        if (!user.password_hash) {
+            throw new Error("Password hash not found for this user");
         }
+
+        const isPasswordValid = await bcrypt.compare(
+            password,
+            user.password_hash
+        );
+
+        if (!isPasswordValid) {
+            throw new Error("Invalid password");
+        }
+
+        const JWT_SECRET = process.env.JWT_SECRET;
+
+        if (!JWT_SECRET) {
+            throw new Error("JWT_SECRET is not configured");
+        }
+
+        const token = jwt.sign(
+            {
+                user_id: user._id,
+                email: user.email
+            },
+            JWT_SECRET,
+            {
+                expiresIn: "1d"
+            }
+        );
+
+        return {
+            access_token: token,
+            token_type: "Bearer",
+            expires_in: "1d",
+            user: {
+                user_id: user._id,
+                email: user.email
+            }
+        };
     };
-};
 
-export const createUser = async (data: any) => {
-    const existingUser = await userModel.get({
-        email: data.email
-    });
 
-    if (existingUser) {
-        throw new Error("Email already exists");
-    }
+    // CREATE
+    async createUser(data: any) {
+        const existingUser = await userModel.get({
+            email: data.email
+        });
 
-    const passwordHash = await bcrypt.hash(
-        data.password,
-        10
-    );
-
-    const user = await userModel.add({
-        first_name: data.first_name,
-        last_name: data.last_name,
-        email: data.email,
-        mobile_number: data.mobile_number,
-        password_hash: passwordHash
-    });
-
-    // Publish Kafka event
-    await publishKafkaMessage(
-        USER_TOPICS.CREATED,
-        {
-            user_id: user._id.toString(),
-            first_name: user.first_name,
-            last_name: user.last_name,
-            email: user.email,
-            mobile_number: user.mobile_number
-        },
-         user._id.toString()
-    );
-
-    return user;
-};
-
-export const getUsers = async () => {
-    const users = await userModel.getAll();
-    return users;
-};
-
-export const getUserById = async (id: string) => {
-    const user = await userModel.get({_id: id})
-    if (!user) {
-        throw new Error("User not found");
-    }
-    return user;
-};
-
-export const updateUser = async (id: string, data: any) => {
-    // Check duplicate email
-    if (data.email) {
-     const emailTaken = await userModel.get({ email: data.email, _id: { $ne: id } });
-        if (emailTaken) {
-            throw new Error("Email is already in use by another account");
+        if (existingUser) {
+            throw new Error("Email already exists");
         }
-    }
 
-    const updateData = { ...data };
-    if (data.password) {
-        updateData.password_hash = await bcrypt.hash(data.password, 10);
-        delete updateData.password;
-    }
+        const passwordHash = await bcrypt.hash(
+            data.password,
+            10
+        );
 
-    const updatedUser = await userModel.update({_id: id}, updateData);
+        const user = await super.create({
+            first_name: data.first_name,
+            last_name: data.last_name,
+            email: data.email,
+            mobile_number: data.mobile_number,
+            password_hash: passwordHash
+        });
 
-    if (!updatedUser) {
-        throw new Error("User not found to update");
-    }
+        // Publish Kafka event
+        await publishKafkaMessage(
+            USER_TOPICS.CREATED,
+            {
+                user_id: user._id.toString(),
+                first_name: user.first_name,
+                last_name: user.last_name,
+                email: user.email,
+                mobile_number: user.mobile_number
+            },
+             user._id.toString()
+        );
 
-    // Publish Kafka event
-    await publishKafkaMessage(
-        USER_TOPICS.UPDATED,
-        {
-            user_id: updatedUser._id.toString(),
-            first_name: updatedUser.first_name,
-            last_name: updatedUser.last_name,
-            email: updatedUser.email,
-            mobile_number: updatedUser.mobile_number
-        },
-        id 
-    );
-
-    return updatedUser;
-};
-
-export const deleteUser = async (id: string) => {
-    const user = await userModel.get({_id: id});
-    if (!user) {
-        throw new Error("User not found to delete");
-    }
-
-    await userModel.delete({_id: id});
-
-    // Publish Kafka event
-    await publishKafkaMessage(
-        USER_TOPICS.DELETED,
-        {
-            user_id: user._id.toString(),
-            first_name: user.first_name,
-            last_name: user.last_name,
-            email: user.email,
-            mobile_number: user.mobile_number
-        },
-        id 
-    );
-
-    return {
-        id: user._id,
-        message: "User deleted from database successfully"
+        return user;
     };
-};
+
+
+    // GET USERS
+    async getUsers() {
+        const users = await userModel.getAll();
+        return users;
+    };
+
+
+    // GET BY ID
+    async getUserById(id: string) {
+        const user = await super.getById(id)
+        if (!user) {
+            throw new Error("User not found");
+        }
+        return user;
+    };
+
+    
+    // UPDATE 
+    async updateUser(id: string, data: any) {
+        // Check duplicate email
+        if (data.email) {
+         const emailTaken = await userModel.get({ email: data.email, _id: { $ne: id } });
+            if (emailTaken) {
+                throw new Error("Email is already in use by another account");
+            }
+        }
+
+        const updateData = { ...data };
+        if (data.password) {
+            updateData.password_hash = await bcrypt.hash(data.password, 10);
+            delete updateData.password;
+        }
+
+        const updatedUser = await super.update(id, updateData);
+
+        if (!updatedUser) {
+            throw new Error("User not found to update");
+        }
+
+        // Publish Kafka event
+        await publishKafkaMessage(
+            USER_TOPICS.UPDATED,
+            {
+                user_id: updatedUser._id.toString(),
+                first_name: updatedUser.first_name,
+                last_name: updatedUser.last_name,
+                email: updatedUser.email,
+                mobile_number: updatedUser.mobile_number
+            },
+            id 
+        );
+
+        return updatedUser;
+    };
+
+
+    // DELETE
+    async deleteUser(id: string) {
+        const user = await userModel.get({_id: id});
+        if (!user) {
+            throw new Error("User not found to delete");
+        }
+
+        await super.delete(id);
+
+        // Publish Kafka event
+        await publishKafkaMessage(
+            USER_TOPICS.DELETED,
+            {
+                user_id: user._id.toString(),
+                first_name: user.first_name,
+                last_name: user.last_name,
+                email: user.email,
+                mobile_number: user.mobile_number
+            },
+            id 
+        );
+
+        return {
+            id: user._id,
+            message: "User deleted from database successfully"
+        };
+    };
+}
+
+export const userService = new UserService();
