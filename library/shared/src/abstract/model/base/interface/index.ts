2f9b6ab6caa85fc40590a75d76d870a723e1dafa
binalpatel02 <binal.patel@saeculumsolutions.com>
2026-09-25 10:42:26 +0530

    remove unused data

--- library/shared/src/abstract/model/base/interface/index.ts
+++ library/shared/src/abstract/model/base/interface/index.ts
@@ -1,17 +1,4 @@
 export interface IModelOptions {
-    disable_delete?: boolean;
-    disable_update?: boolean;
-    disable_status?: boolean;
-
-    enable_auto?: boolean;
-
     exclusion?: Record<string, number>;
-
-    hard_delete?: boolean;
-
-    indexed_fields?: string[];
-
-    lookup_fields?: Record<string, number>;
-
     return_doc?: boolean;
 }
--- library/shared/src/abstract/model/base/util/indec.ts
+++ library/shared/src/abstract/model/base/util/indec.ts
@@ -1,9 +1,5 @@
 export interface IQueryOptions {
     filter? : Record<string, unknown>;
-    projection? : Record<string, number>;
-    sort?: Record<string, 1 | -1>;
-    limit?: number;
-    skip?: number;
 }
 
 export class AbstractUtil {
@@ -14,24 +10,4 @@ export class AbstractUtil {
         return filter
     } 
 
-    prepareProjection(
-        projection: Record<string, number> = {}
-    ) {
-        return projection
-    }
-
-    prepareSort(
-        sort: Record<string, 1| -1> = {}
-    ) {
-        return sort
-    }
-
-    preparePagination(
-        skip = 0,
-        limit = 20
-    ) {
-        return {
-            skip, limit
-        }
-    }
 }
